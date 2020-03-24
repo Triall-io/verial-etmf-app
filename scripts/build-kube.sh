ddb4ab027fb1cee6ebaa77afdcffb41e736b1865
LDMGN <lgijsen@sphereon.com>
2020-03-24 10:58:31 +0100

    Fix script docker build

--- scripts/build-kube.sh
+++ scripts/build-kube.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-show_help() {
-    echo "Usage: build-kube.sh <image-tag>"
+if [[ ! ("$#" == 1) ]]; then
+    echo "Usage: $0 <image-tag>>"
     echo ""
     echo "Required environment variables:"
-    echo "$REGISTRY_URL"
-    echo "$REGISTRY_USER"
-    echo "$REGISTRY_SECRET"
-}
-
-
-while [[ $1 == -* ]]; do
-    case "$1" in
-      -h|--help|-\?) show_help; exit 0;;
-      -*) echo "invalid option: $1" 1>&2; show_help; exit 1;;
-    esac
-done
+    echo "\$REGISTRY_URL"
+    echo "\$REGISTRY_USER"
+    echo "\$REGISTRY_SECRET"
+    exit 1
+fi
 
 docker=$(which docker)
 
 tag=$1
 
 echo "Logging docker into $REGISTRY_URL"
-$docker login $REGISTRY_URL
+$docker login $REGISTRY_URL -u $REGISTRY_USER -p $REGISTRY_SECRET || exit;
 
 echo "Building and uploading docker image"
 $docker build -t triall.com/alfresco/adf-demo:$tag .
-$docker tag triall.com/alfresco/adf-demo:$tag $REGISTRY_URL/alfresco/adf-demo
+$docker tag triall.com/alfresco/adf-demo:$tag $REGISTRY_URL/alfresco/adf-demo:$tag
 $docker push $REGISTRY_URL/alfresco/adf-demo:$tag
